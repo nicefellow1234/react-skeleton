887cdeddcca2529f2698a993231547cb32f81b31
Umair Shah Yousafzai <nicefellow1234@gmail.com>
2016-06-12 16:38:37 +0500

    Modified files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 "use strict";
 
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var open = require('gulp-open');
+var connect = require('gulp-connect'); // Runs a local Dev Server
+var open = require('gulp-open'); // Open a URL in a Web Browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use Conventional text strams with Gulp
+var concat = require('gulp-concat'); // Concatenates files
+var lint = require('gulp-eslint'); // Lint JS files, including JSX
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http:localhost',
 	paths: {
 		html: './src/*.html',
-		dist: './dist'
+		js: './src/**/*.js',
+		css : [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
+		dist: './dist',
+		mainJs: './src/main.js'
 	}
 }
 
@@ -23,8 +34,8 @@ gulp.task('connect', function(){
 });
 
 gulp.task('open', ['connect'], function() {
-	gulp.src('dist/index.html')
-	.pipe(open('', { url : config.devBaseUrl + ':' + config.port + '/'}))
+	gulp.src(__filename)
+	.pipe(open({ uri : config.devBaseUrl + ':' + config.port + '/'}))
 
 });
 
@@ -34,8 +45,36 @@ gulp.task('html', function() {
 	.pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
+	.transform(reactify)
+	.bundle()
+	.on('error',function() { 
+		console.error(console)})
+	.pipe(source('bundle.js'))
+	.pipe(gulp.dest(config.paths.dist + '/scripts'))
+	.pipe(connect.reload());
 });
 
-gulp.task('default',['html','open','watch']);
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+	.pipe(concat('bundle.css'))
+	.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+	return gulp
+	.src(config.paths.js)
+	.pipe(lint({config: 'eslint.config.json'}))
+	.pipe(lint.format())
+	.pipe(lint.failAfterError());
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js','lint']);
+});
+
+gulp.task('default',['html','js','css','lint','open','watch']);
---
+++ src/main.js
@@ -0,0 +1,6 @@
+$ = jQuery = require('jquery');
+
+
+var App = console.log('Hello World from Browserify');
+
+module.exports = App;
